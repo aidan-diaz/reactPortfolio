d8fbb32296e3f84d00900803f1332f464fb88798
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 06:21:13 -0500

    added netlify attribute to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = ({ darkMode }) => {
     return (
         <section className='contactFormSection'>
             <h3 className='contactFormTitle'>Please Contact me Below with any Questions!</h3>
-            <form className='contactForm' action="POST" name='contactForm'>
+            <form className='contactForm' action="POST" name='contactForm' netlify>
                 <div className='senderInfo'>
                     <input type="text" name='name' placeholder='Name' />
                     <input type="text" name='email' placeholder='Email' />
