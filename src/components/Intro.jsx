298aacca7be2c393c178ef85afa86172b920697a
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 05:01:46 -0500

    updated colors for dark and light mode

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,8 +3,8 @@ import { HeadShot } from "./Headshot"
 const Intro = () => {
     return (
         <section className='introSection'>
-            <h2>Hello! My name is Aidan Diaz - I am a Fullstack Web Developer</h2>
             <HeadShot />
+            <h1>Hello! My name is Aidan Diaz - I am a Fullstack Web Developer</h1>
         </section>
     )
 }
