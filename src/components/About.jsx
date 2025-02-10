3d2801dfb6fe0b24ebf2f3436db101e3126a3a40
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 05:04:31 -0500

    styling updates to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 const About = () => {
     return (
         <section className='aboutMeContainer'>
+            <h3 className='aboutMeHeader'>About Me</h3>
             <p className='aboutMeParagraph'>In my previous career, I discovered that helping others succeed is one of the most rewarding aspects of any job. As the primary mentor for onboarding new hires and showing them how to use our newly integrated systems, I learned the importance of patience, clear communication, and adaptability. Now, as a software engineer, I bring that same passion for teaching and support to my team. I thrive on late nights spent guiding my colleagues through complex problems and uncovering solutions as a team. I believe that true teamwork is about lifting each other up, and I carry that mindset into every project I work on. I am not only driven by my own growth, but by the growth of my colleagues. I am excited to bring my collaborative spirit, to take on the challenges of the tech industry with a team that values both individual and collective growth.</p>
         </section>
     )
