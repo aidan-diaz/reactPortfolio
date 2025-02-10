2b2b8b38d0b953ac074a71592bc10fb7e94c57ae
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 06:00:29 -0500

    added SubmitButton.jsx component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,13 @@ const Contact = () => {
     return (
         <section className='contactFormSection'>
             <h3 className='contactFormTitle'>Please Contact me Below with any Questions!</h3>
-            <form className='contactForm' action="POST">
+            <form className='contactForm' action="POST" name='contactForm'>
                 <div className='senderInfo'>
-                    <input type="text" placeholder='Name' />
-                    <input type="text" placeholder='Email' />
+                    <input type="text" name='name' placeholder='Name' />
+                    <input type="text" name='email' placeholder='Email' />
                 </div>
                 <div className='senderMessage'>
-                    <textarea placeholder='Message' />
+                    <textarea name='message' placeholder='Message' />
                 </div>
                 {/* submit button goes here */}
             </form>
