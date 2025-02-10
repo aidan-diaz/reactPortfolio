a2509d91398be9ed07d305f4aaad0695c3324ac5
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 13:57:38 -0500

    added icons to darkmode button

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react'
 const DarkModeButton = ({darkMode, toggleMode}) => {
     console.log(darkMode)
 
+    let iconClass = darkMode ? "bi bi-sun" : "bi bi-moon-stars-fill"
+
     return (
-        <button className={darkMode ? 'lightMode' : 'darkMode'} onClick={ toggleMode }>Turn {darkMode ? 'off' : 'on'} Dark Mode</button>
+        <button className={darkMode ? 'lightMode' : 'darkMode'} onClick={ toggleMode }>Turn {darkMode ? 'off' : 'on'} Dark Mode <i className={iconClass}></i></button>
     )
 
 }
