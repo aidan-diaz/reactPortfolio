d51a65f0724b53f0ed2bd331d92cf790fbb28460
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 06:47:17 -0500

    general syntax updates

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -2,7 +2,7 @@ const SubmitButton = ({ darkMode }) => {
     console.log('button', darkMode)
     const submitButtonClasses = `submitButton ${darkMode ? 'lightMode' : 'darkMode'}`
     return (
-        <input className={submitButtonClasses} type='submit' value='Send Message'>Send Message</input>
+        <input className={submitButtonClasses} type='submit' value='Send Message' />
     )
 }
 
