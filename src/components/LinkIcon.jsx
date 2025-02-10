e0553b850d88301ac9a86546f58cbe8ca66baeaf
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 14:31:01 -0500

    updated styling for header

--- src/components/LinkIcon.jsx
+++ src/components/LinkIcon.jsx
@@ -1,7 +1,8 @@
 const LinkIcon = (props) => {
     console.log(props.icon.className)
+    const classes = `headerIcon ${props.icon.className}`
     return (
-        <li className={props.icon.className}></li>
+        <li className={classes}></li>
     )
 }
 
