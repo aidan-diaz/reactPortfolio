23145ca904185c81edf182878d169c0d6becfba9
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 04:49:41 -0500

    updated styling for headshot

---
+++ src/components/Headshot.jsx
@@ -0,0 +1,9 @@
+const HeadShot = () => {
+    return (
+        <div className='headshotContainer'>
+            <img className='headshot' src='headshot.jpg' alt="Aidan Diaz Headshot" />
+        </div>
+    )
+}
+
+export { HeadShot }
