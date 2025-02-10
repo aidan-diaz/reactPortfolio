e813826fcfec1adcd8e3a353b479082a8886c027
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 14:54:52 -0500

    updated styling to toggle button

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -3,10 +3,11 @@ import { useState } from 'react'
 const DarkModeButton = ({darkMode, toggleMode}) => {
     console.log(darkMode)
 
+    let buttonClass = `toggleButton ${darkMode ? 'lightMode' : 'darkMode'}`
     let iconClass = darkMode ? "bi bi-sun" : "bi bi-moon-stars-fill"
 
     return (
-        <button className={darkMode ? 'lightMode' : 'darkMode'} onClick={ toggleMode }>Turn {darkMode ? 'off' : 'on'} Dark Mode <i className={iconClass}></i></button>
+        <button className={buttonClass} onClick={ toggleMode }>Turn {darkMode ? 'off' : 'on'} Dark Mode <i className={iconClass}></i></button>
     )
 
 }
