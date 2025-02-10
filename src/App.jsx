34304d8d7916053cc89ef84558866ccbb02e9fda
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-10 00:27:48 -0500

    created a css folder and moved css files to it. Updated imports for css files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import './css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>This is my portfolio</h1>
     </div>
   )
 }
